7f09c07f87fabe67fd6bd0e0933d181c5f860e8f
Apurva <apurvabhavsar31@gmail.com>
2023-06-24 14:23:41 +0530

    home page finished

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,18 @@ import Hero from "public/hero.png";
 export default function Home() {
   return (
     <main className={styles.container}>
-      <Image src={Hero} alt="Hero" />
+      <section className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, sint!
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </section>
+      <section className={styles.item}>
+        <Image src={Hero} alt="Hero" className={styles.heroImg} />
+      </section>
     </main>
   );
 }
