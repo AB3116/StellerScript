ea4c7486c6ef034e4c1979e07c287d5e5f10cad5
Apurva <apurvabhavsar31@gmail.com>
2023-06-24 22:28:05 +0530

    portfolio finished

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -6,22 +6,25 @@ import Button from "@/components/button/Button";
 function Contact() {
   return (
     <main className={styles.container}>
-      <section className={styles.imageContainer}>
-        <Image
-          src={ContactImg}
-          alt="Contact"
-          fill={true}
-          className={styles.img}
-        />
-      </section>
-      <section className={styles.formContainer}>
-        <form className={styles.form}>
-          <input type="text" className={styles.input} placeholder="Name" />
-          <input type="text" className={styles.input} placeholder="Email" />
-          <textarea className={styles.textarea} placeholder="Message" />
-          <Button url="#" text="Send" />
-        </form>
-      </section>
+      <h1 className={styles.title}>Let&apos;s Keep in Touch</h1>
+      <div className={styles.imageFormContainer}>
+        <section className={styles.imageContainer}>
+          <Image
+            src={ContactImg}
+            alt="Contact"
+            fill={true}
+            className={styles.img}
+          />
+        </section>
+        <section className={styles.formContainer}>
+          <form className={styles.form}>
+            <input type="text" className={styles.input} placeholder="Name" />
+            <input type="text" className={styles.input} placeholder="Email" />
+            <textarea className={styles.textarea} placeholder="Message" />
+            <Button url="#" text="Send" />
+          </form>
+        </section>
+      </div>
     </main>
   );
 }
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import Link from "next/link";
+import styles from "./page.module.css";
 
 function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <menu className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations" className={styles.item}>
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link href="/portfolio/websites" className={styles.item}>
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link href="/portfolio/application" className={styles.item}>
+          <span className={styles.title}>Applications</span>
+        </Link>
+      </div>
+    </menu>
+  );
 }
 
 export default Portfolio;
