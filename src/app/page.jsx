b760b4753a1b30f5fd1a8af859bcd19f6babe208
Apurva <apurvabhavsar31@gmail.com>
2023-06-24 12:15:27 +0530

    hero

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Hero from "public/hero.png";
 
 export default function Home() {
   return (
-    <main>
-      Home
+    <main className={styles.container}>
+      <Image src={Hero} alt="Hero" />
     </main>
-  )
+  );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,40 @@
-import styles from "./Footer.module.css";
+import Image from "next/image";
+import styles from "./footer.module.css";
 
 function Footer() {
   return (
-    <footer>
-      <p>&copy;2023 F1. All rights reserved.</p>
+    <footer className={styles.container}>
+      <p>&copy;2023 StellerScript. All rights reserved.</p>
+      <div className={styles.socials}>
+        <Image
+          src="/fb.png"
+          width={16}
+          height={16}
+          className={styles.icon}
+          alt="StellerScript Facebook"
+        />
+        <Image
+          src="/ins.png"
+          width={16}
+          height={16}
+          className={styles.icon}
+          alt="StellerScript Instagram"
+        />
+        <Image
+          src="/twt.png"
+          width={16}
+          height={16}
+          className={styles.icon}
+          alt="StellerScript Twitter"
+        />
+        <Image
+          src="/yt.png"
+          width={16}
+          height={16}
+          className={styles.icon}
+          alt="StellerScript YouTube"
+        />
+      </div>
     </footer>
   );
 }
