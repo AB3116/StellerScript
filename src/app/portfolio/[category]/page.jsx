34f7a210c50b1da2a58f01934cedefc2f377a064
Apurva <apurvabhavsar31@gmail.com>
2023-06-25 19:35:16 +0530

    client side data fetching in portfolio

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,49 +1,39 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Button from "@/components/button/Button";
+import { items } from "./data";
+import { notFound } from "next/navigation";
+
+function getData(cat) {
+  const data = items[cat];
+
+  if (data) return data;
+
+  return notFound();
+}
 
 function Category({ params }) {
+  const data = getData(params.category);
   return (
     <main className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <section className={styles.item}>
-        <div className={styles.content}>
-          <h2 className={styles.title}>Lorem ipsum dolor sit</h2>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut tempore
-            eligendi alias voluptatibus aliquid? Delectus facilis unde tenetur
-            sequi minus.
-          </p>
-          <Button url="#" text="See More" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            fill={true}
-            src="https://images.pexels.com/photos/1418595/pexels-photo-1418595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt="Illustration"
-          />
-        </div>
-      </section>
-      <section className={styles.item}>
-        <div className={styles.content}>
-          <h2 className={styles.title}>Lorem ipsum dolor sit</h2>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut tempore
-            eligendi alias voluptatibus aliquid? Delectus facilis unde tenetur
-            sequi minus.
-          </p>
-          <Button url="#" text="See More" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            fill={true}
-            src="https://images.pexels.com/photos/1418595/pexels-photo-1418595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt="Illustration"
-          />
-        </div>
-      </section>
+      {data.map((item) => (
+        <section className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h2 className={styles.title}>{item.title}</h2>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button url="#" text="See More" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.img}
+              fill={true}
+              src={item.image}
+              alt="Illustration"
+            />
+          </div>
+        </section>
+      ))}
     </main>
   );
 }
