808df13e20bab72914fda844df5e51680703d3ba
Apurva <apurvabhavsar31@gmail.com>
2023-06-29 16:48:27 +0530

    styled login page

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,9 +1,28 @@
 "use client";
 import { signIn } from "next-auth/react";
 import React from "react";
+import Link from "next/link";
+import styles from "./page.module.css";
 
 function Login() {
-  return <button onClick={() => signIn("google")}>Login with Google</button>;
+  return (
+    <>
+      <div className={styles.container}>
+        <form className={styles.formContainer}>
+          <input type="email" placeholder="Email" className={styles.input} />
+          <input
+            type="password"
+            placeholder="Password"
+            className={styles.input}
+          />
+          <button type="submit" className={styles.registerButton}>
+            Login
+          </button>
+          <button onClick={() => signIn("google")} className={styles.registerButton}>Login with Google</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Login;
