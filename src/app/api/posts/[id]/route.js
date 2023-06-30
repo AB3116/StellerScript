85184f73c2d545eea10ab44c387d9240d54ee69a
Apurva <apurvabhavsar31@gmail.com>
2023-06-30 09:57:15 +0530

    finished

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -3,11 +3,22 @@ import { NextResponse } from "next/server";
 import connect from "@/utils/db";
 
 export const GET = async (request, { params }) => {
-    const {id} = params;
+  const { id } = params;
   try {
     await connect();
     const post = await Post.findById(id);
     return new NextResponse(JSON.stringify(post), { status: 200 });
+  } catch (err) {
+    return new NextResponse("Database error", { status: 500 });
+  }
+};
+
+export const DELETE = async (request, { params }) => {
+  const { id } = params;
+  try {
+    await connect();
+    await Post.findByIdAndDelete(id);
+    return new NextResponse("Post has been deleted", { status: 200 });
   } catch (err) {
     return new NextResponse("Database error", { status: 500 });
   }
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,14 +3,15 @@ import { useSession } from "next-auth/react";
 import styles from "./page.module.css";
 import useSWR from "swr";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 function Dashboard() {
   const session = useSession();
   const router = useRouter();
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  const { data, error, isLoading } = useSWR(
-    "https://jsonplaceholder.typicode.com/posts",
+  const { data, mutate, error, isLoading } = useSWR(
+    `/api/posts?username=${session?.data?.user.name}`,
     fetcher
   );
 
@@ -22,8 +23,83 @@ function Dashboard() {
     router?.push("/dashboard/login");
   }
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const title = event.target[0].value;
+    const desc = event.target[1].value;
+    const img = event.target[2].value;
+    const content = event.target[3].value;
+
+    try {
+      await fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          desc,
+          img,
+          content,
+          username: session.data.user.name,
+        }),
+      });
+      mutate();
+      event.target.reset();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`/api/posts/${id}`, {
+        method: "DELETE",
+      });
+      mutate();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   if (session.status === "authenticated") {
-    return <div className={styles.container}>Dashboard</div>;
+    return (
+      <div className={styles.container}>
+        <div className={styles.posts}>
+          {isLoading
+            ? "Loading..."
+            : data.map((post) => (
+                <div key={post._id} className={styles.post}>
+                  <div className={styles.imgContainer}>
+                    <Image src={post.img} alt="" width={200} height={100} />
+                  </div>
+                  <h2 className={styles.postTitle}></h2>
+                  <span
+                    className={styles.delete}
+                    onClick={() => handleDelete(post.id)}
+                  >
+                    x
+                  </span>
+                </div>
+              ))}
+        </div>
+        <form className={styles.new} onSubmit={handleSubmit}>
+          <h1>Add New Post</h1>
+          <input type="text" placeholder="Title" className={styles.input} />
+          <input
+            type="text"
+            placeholder="Description"
+            className={styles.input}
+          />
+          <input type="text" placeholder="Image" className={styles.input} />
+          <textarea
+            type="text"
+            placeholder="Content"
+            className={styles.textarea}
+          />
+          <button type="submit" className={styles.button}>
+            Send
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
