b3cf02486912d30b76136f146a67388cc3ca9c3d
Apurva <apurvabhavsar31@gmail.com>
2023-07-08 10:56:42 +0530

    final

--- src/app/portfolio/layout.jsx
+++ src/app/portfolio/layout.jsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 function PortfolioLayout({ children }) {
   return (
     <div>
-      <h1  className={styles.mainTitle}>Our Works</h1>
+      <h1 className={styles.mainTitle}>Our Works</h1>
       {children}
     </div>
   );
